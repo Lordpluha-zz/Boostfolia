8397f8650630692d149fb7625f24f291f419aa35
Vladislav <Tesluakevlad@gmail.com>
2021-07-21 22:28:08 +0300

    21.07.21
    Вырезал video из section.video (article) для чистоты консоли
    Исправил баги с header(js), clients(js)
    Сделал адаптивный js код для our_works
    Добавил анимацию красивой подгрузки текста

--- src/Landing_page/js/scripts/script.min.js
+++ src/Landing_page/js/scripts/script.min.js
@@ -1,39 +1,75 @@
 jQuery(document).ready(function($) {
 	
-	// Header slider
-	var mrg = -100;
+	console.clear();
 
-	function setML(mrg_) {
+	// Text upploading effect
+	
+	const AnimItems = $('._anim-elem');
+
+	function offset(el) {
+		const rect = el.getBoundingClientRect(),
+			scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+			scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+		return { top: rect.top + scrollTop, left: rect.left + scrollLeft}
+	}
+
+	function animOnScroll () {
+		for (let index = 0; index < AnimItems.length; index++) {
+			const AnimItem = AnimItems[index];
+			const AnimItemHeight = AnimItem.offsetHeight;
+			const AnimItemOffset = offset(AnimItem).top;
+			const animStart = 4;
+
+			let animItemPoint = window.innerHeight - AnimItemHeight / animStart;
+			if (AnimItemHeight > window.innerHeight) {
+				animItemPoint = window.innerHeight - window.innerHeight / animStart;
+			}
+
+			if ((pageYOffset > AnimItemOffset - animItemPoint) && pageYOffset < (AnimItemOffset + AnimItemHeight)) {
+				AnimItem.classList.add('_active__fx');
+			} else {
+				AnimItem.classList.remove('_active__fx');
+			}
+		}
+	}
+
+	$(window).scroll(animOnScroll);
+
+	animOnScroll();
+
+	// Header slider
+	var mrg1 = -100;
+
+	function setML1(mrg_) {
 		$('header .header__slider .slider__tape').css({
 			'margin-left': mrg_+'%'
 		});
 	}
-	setML(mrg);
+	setML1(mrg1);
 
 	$('header .header__slider .slider__ctrl .slider__ctrl_to_slide button.left').click(function(event) {
-		mrg += 100;
+		mrg1 += 100;
 		
-		if (mrg > 0) {
-			mrg = -200;
-			setML(mrg);
+		if (mrg1 > 0) {
+			mrg1 = -200;
+			setML1(mrg1);
 		} else {
-			setML(mrg);
+			setML1(mrg1);
 		}
 	});
 
 	$('header .header__slider .slider__ctrl .slider__ctrl_to_slide button.right').click(function(event) {
-		mrg -= 100;
+		mrg1 -= 100;
 
-		if (mrg < -200) {
-			mrg = 0;
-			setML(mrg);
+		if (mrg1 < -200) {
+			mrg1 = 0;
+			setML1(mrg1);
 		} else {
-			setML(mrg);
+			setML1(mrg1);
 		}
 	});
 
-	// Nav effx
-
+	// Nav fx
 	window.onscroll = function scrollFunction() {
 		if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
 	    	$('nav #content').css({
@@ -45,36 +81,49 @@ jQuery(document).ready(function($) {
 	    	});
 	  	}
 	}
+	// Article__out_works slider
+	for (let i = 1; i <= $(`article section.our_works #content .our_works__nav button`).length; i++) {
+		$(`article section.our_works #content .our_works__nav .l${i}`).click(
+			function(event) {
+				$('article section.our_works #content .our_works__nav button.active').removeClass('active');
+				$(`article section.our_works #content .our_works__nav .l${i}`).addClass('active');
+				
+				$('article section.our_works .our_works__slider .layer.active').removeClass('active');
+				$(`article section.our_works .our_works__slider .layer${i}`).addClass('active');
+			}
+		);
+	}
+
 
 	// Article__clients slider
-	var mrg = -100;
+	var mrg2 = -100;
 
-	function setML(mrg_) {
+	function setML2(mrg_) {
 		$('article section.clients .clients__slider .slider__tape').css({
 			'margin-left': mrg_+'%'
 		});
 	}
-	setML(mrg);
+	setML2(mrg2);
 
 	$('article section.clients .clients__slider .slider__controls button.left').click(function(event) {
-		mrg += 100;
+		mrg2 += 100;
 		
-		if (mrg > 0) {
-			mrg = -200;
-			setML(mrg);
+		if (mrg2 > 0) {
+			mrg2 = -200;
+			setML2(mrg2);
 		} else {
-			setML(mrg);
+			setML2(mrg2);
 		}
 	});
 
 	$('article section.clients .clients__slider .slider__controls button.right').click(function(event) {
-		mrg -= 100;
+		mrg2 -= 100;
 
-		if (mrg < -200) {
-			mrg = 0;
-			setML(mrg);
+		if (mrg2 < -200) {
+			mrg2 = 0;
+			setML2(mrg2);
 		} else {
-			setML(mrg);
+			setML2(mrg2);
 		}
 	});
 });
